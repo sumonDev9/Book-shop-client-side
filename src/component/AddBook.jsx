669d1feda880e71e7e1345a04db8f73e9ee5c75d
sumonDev9 <sumonmit678@gmail.com>
2024-12-01 13:48:15 +0530

    addbook components to server side data send

--- src/component/AddBook.jsx
+++ src/component/AddBook.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
+import Swal from 'sweetalert2';
 
 const AddBook = () => {
+
+    const handleAddBook = e => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const author = form.author.value;
+        const price = form.price.value;
+        const photo = form.photo.value;
+
+        const newBook = {name, author, price, photo};
+       
+        // send to data server
+        fetch("http://localhost:5000/book", {
+            method:'POST',
+            headers: {
+                'content-type' : 'application/json'
+            },
+            body: JSON.stringify(newBook)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            form.reset();
+            if(data.insertedId){
+                Swal.fire({
+                    title: 'Success!',
+                    text: 'The book has been added successfully.',
+                    icon: 'success',
+                    confirmButtonText: 'OK'
+                });
+            }
+        })
+    }
+
     return (
         <div>
             <h1 className='text-primary font-bold text-2xl ml-12 pt-5'>Add Book</h1>
             <div className='max-w-5xl p-10 mx-auto rounded-xl mt-10 bg-white shadow-lg'>
-                <form>
+                <form onSubmit={handleAddBook}>
                     <div className='mx-auto flex gap-5'>
                         <div className="form-control md:w-1/2">
                             <label className="label">
@@ -17,7 +52,7 @@ const AddBook = () => {
                             <label className="label">
                                 <span className="label-text text-primary text-lg font-bold">Author Name</span>
                             </label>
-                            <input type="text" name="name" placeholder="Author Name" className="input w-full input-bordered" />
+                            <input type="text" name="author" placeholder="Author Name" className="input w-full input-bordered" />
                         </div>
                     </div>
                     <div className='mx-auto flex mt-6 gap-5'>
@@ -25,17 +60,17 @@ const AddBook = () => {
                             <label className="label">
                                 <span className="label-text text-primary text-lg font-bold">Add Price</span>
                             </label>
-                            <input type="number" name="name" placeholder="Enter Price" className="input w-full input-bordered" />
+                            <input type="number" name="price" placeholder="Enter Price" className="input w-full input-bordered" />
                         </div>
                         <div className="form-control md:w-1/2">
                             <label className="label">
                                 <span className="label-text text-primary text-lg font-bold">Add Book Cover Photo</span>
                             </label>
-                            <input type="text" name="name" placeholder="photo url" className="input w-full input-bordered" />
+                            <input type="text" name="photo" placeholder="photo url" className="input w-full input-bordered" />
                         </div>
                     </div>
                     <div className='flex justify-end'>
-                        <button type='submit' className='bg-bgbton text-white rounded-md p-2 mt-5'>Add Book</button>
+                        <button  className='bg-bgbton text-white rounded-md p-2 mt-5'>Add Book</button>
                     </div>
                 </form>
             </div>
