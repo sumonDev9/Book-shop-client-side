a7535473330a4719c01525005325f48888da1cd0
sumonDev9 <sumonmit678@gmail.com>
2024-12-01 10:37:18 +0530

    bannar design updated

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -23,17 +23,17 @@ const Banner = () => {
           <SwiperSlide>
             <div className="relative">
               <img
-                src="https://i.ibb.co/tX1YL9N/mountains-2368817-1280-1.jpg"
+                src="https://i.ibb.co/5sn8PPs/img11.webp"
                 alt="Adventure 1"
                 className="w-full h-64 sm:h-80 md:h-96 lg:h-[450px] object-cover"
               />
-              <div className="absolute inset-0 bg-black bg-opacity-60"></div>
+              {/* <div className="absolute inset-0 bg-black bg-opacity-60"></div> */}
               <div className="absolute inset-0 flex justify-center items-center text-center text-white">
                 <div>
-                  <h2 className="text-base md:text-2xl lg:text-4xl font-bold animate__animated animate__fadeInUp animate__delay-1s">Discover Adventure with MountainVista</h2>
-                  <p className="mt-2 max-w-3xl text-xm md:text-base lg:text-lg font-medium animate__animated animate__fadeInUp animate__delay-2s">Conquer mountain peaks with expert-led expeditions, offering thrilling adventures to breathtaking, remote destinations worldwide.</p>
-                  <button className="mt-4 px-6 py-2 bg-bgbton text-primary font-semibold rounded text-sm sm:text-base lg:text-lg animate__animated animate__fadeInUp animate__delay-3s">
-                    Start Your Journey
+                  <h2 className="text-base md:text-2xl lg:text-4xl font-bold ">The Art of Storytelling</h2>
+                  <p className="mt-2 max-w-3xl text-xm md:text-base lg:text-lg font-medium ">Discover the secrets to crafting compelling narratives that captivate readers worldwide.</p>
+                  <button className="mt-4 px-6 py-2 bg-bgbton text-primary font-semibold rounded text-sm sm:text-base lg:text-lg">
+                  Learn More
                   </button>
                 </div>
               </div>
@@ -44,17 +44,17 @@ const Banner = () => {
           <SwiperSlide>
             <div className="relative">
               <img
-                src="https://i.ibb.co/0JJgRnp/mountains-1673918-960-720.jpg"
+                src="https://i.ibb.co/2YJmNZ2/book1.jpg"
                 alt="Adventure 1"
                 className="w-full h-64 sm:h-80 md:h-96 lg:h-[450px] object-cover"
               />
-              <div className="absolute inset-0 bg-black bg-opacity-60"></div>
+              {/* <div className="absolute inset-0 bg-black bg-opacity-60"></div> */}
               <div className="absolute inset-0 flex justify-center items-center text-center text-white">
                 <div>
-                  <h2 className="text-base md:text-2xl lg:text-4xl font-bold animate__animated animate__fadeInUp animate__delay-1s">Unleash Your Explorer Spirit</h2>
-                  <p className="mt-2 max-w-3xl  text-xm md:text-base lg:text-lg font-medium animate__animated animate__fadeInUp animate__delay-2s">Explore snow-capped peaks and rugged trails, designed for adventurers seeking challenges and unique outdoor experiences.</p>
-                  <button className="mt-4 px-6 py-2 bg-bgbton text-primary font-semibold rounded text-sm sm:text-base lg:text-lg animate__animated animate__fadeInUp animate__delay-3s">
-                    Book Your Expedition
+                  <h2 className="text-base md:text-2xl lg:text-4xl font-bold">Journey Through Classics</h2>
+                  <p className="mt-2 max-w-3xl  text-xm md:text-base lg:text-lg font-medium ">A curated collection of timeless literary masterpieces that shaped the world of fiction</p>
+                  <button className="mt-4 px-6 py-2 bg-bgbton text-primary font-semibold rounded text-sm sm:text-base lg:text-lg ">
+                  Explore Collection
                   </button>
                 </div>
               </div>
@@ -64,17 +64,18 @@ const Banner = () => {
           <SwiperSlide>
             <div className="relative">
               <img
-                src="https://i.ibb.co/qmb9N54/le1-10.jpg"
+                src="https://i.ibb.co/rsGjyv7/book3.jpg"
                 alt="Adventure 1"
                 className="w-full h-64 sm:h-80 md:h-96 lg:h-[450px] object-cover"
               />
-              <div className="absolute inset-0 bg-black bg-opacity-60"></div>
+              {/* <div className="absolute inset-0 bg-black bg-opacity-60"></div> */}
               <div className="absolute inset-0 flex justify-center items-center text-center text-white">
                 <div>
-                  <h2 className="text-base md:text-2xl  lg:text-4xl font-bold animate__animated animate__fadeInUp animate__delay-1s">Conquer New Heights with MountainVista</h2>
-                  <p className="mt-2 max-w-3xl text-xm md:text-base lg:text-lg font-medium animate__animated animate__fadeInUp animate__delay-2s">Venture off the path into mountains, where curated expeditions blend scenic beauty with thrilling adventures.</p>
-                  <button className="mt-4 px-6 py-2 bg-bgbton text-primary font-semibold rounded text-sm sm:text-base lg:text-lg animate__animated animate__fadeInUp animate__delay-3s">
-                    Join the Expedition
+                  <h2 className="text-base md:text-2xl  lg:text-4xl font-bold">Adventures Await</h2>
+                  <p className="mt-2 max-w-3xl text-xm md:text-base lg:text-lg font-medium ">Dive into thrilling tales of adventure and exploration that ignite your imagination.
+                   Start Reading</p>
+                  <button className="mt-4 px-6 py-2 bg-bgbton text-primary font-semibold rounded text-sm sm:text-base lg:text-lg">
+                  Start Reading
                   </button>
                 </div>
               </div>
